7b083eaee1fe835a34d9b9e6966e5c49011a0972
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2019-03-19 11:06:13 +0300

    Disable analytics by default

--- EarlGrey/Common/GREYConfiguration.m
+++ EarlGrey/Common/GREYConfiguration.m
@@ -60,7 +60,7 @@ NSString *const kGREYConfigKeyArtifactsDirLocation = @"GREYConfigKeyArtifactsDir
                                                                YES);
     GREYFatalAssertWithMessage(searchPaths.count > 0, @"Couldn't find a valid documents directory");
     [self setDefaultValue:searchPaths.firstObject forConfigKey:kGREYConfigKeyArtifactsDirLocation];
-    [self setDefaultValue:@YES forConfigKey:kGREYConfigKeyAnalyticsEnabled];
+    [self setDefaultValue:@NO forConfigKey:kGREYConfigKeyAnalyticsEnabled];
     [self setDefaultValue:@YES forConfigKey:kGREYConfigKeyActionConstraintsEnabled];
     [self setDefaultValue:@(30) forConfigKey:kGREYConfigKeyInteractionTimeoutDuration];
     [self setDefaultValue:@(10) forConfigKey:kGREYConfigKeyCALayerMaxAnimationDuration];
